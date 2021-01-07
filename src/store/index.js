21acf6b74ae24e2cc516ac4c373a5b981fbd15cd
Vladislav <vt118@mail.ru>
2021-01-07 23:48:16 +0300

    feat: fix eslint errors

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,16 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 import auth from './modules/auth'
 import task from './modules/task'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
     auth,
-    task
+    task,
   },
-});
+})
